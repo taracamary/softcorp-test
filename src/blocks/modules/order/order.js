8f9561b9b32ae0b945870c440b1b5705c7c1703a
Maria Tarasenko <taracamary@gmail.com>
2022-07-24 00:17:27 +0300

    add custom scroll bar, fix select bug, add adaptive

---
+++ src/blocks/modules/order/order.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName('order__select-current')[0].onclick = function () {
+    document.getElementsByClassName('order__select-list')[0].classList.toggle('order__select-list--active');
+};
