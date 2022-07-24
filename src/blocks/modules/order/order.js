96a765447f37cac75d1e3f73ca7985aec2e0e4e0
Maria Tarasenko <mariya.tarrassik@gmail.com>
2022-07-24 23:57:05 +0300

    Update 2022-07-24T20:57:01.900Z

--- src/blocks/modules/order/order.js
+++
@@ -1,5 +0,0 @@
-document.getElementsByClassName('payment-form__select-current')[0].onclick = function () {
-    document.getElementsByClassName('payment-form__select-list')[0].classList.toggle('payment-form__select-list--active');
-    document.getElementsByClassName('payment-form__select-icon')[0].classList.toggle('payment-form__select-icon--active');
-    document.getElementsByClassName('payment-form__select-text')[0].classList.toggle('payment-form__select-text--active');
-};
