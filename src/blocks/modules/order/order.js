bc79dde3b92b82ac8c202a5dafb1cf9f3bc7aab6
Maria Tarasenko <taracamary@gmail.com>
2022-07-24 12:54:34 +0300

    add adaptive

--- src/blocks/modules/order/order.js
+++ src/blocks/modules/order/order.js
@@ -1,3 +1,3 @@
-document.getElementsByClassName('order__select-current')[0].onclick = function () {
-    document.getElementsByClassName('order__select-list')[0].classList.toggle('order__select-list--active');
+document.getElementsByClassName('payment-form__select-current')[0].onclick = function () {
+    document.getElementsByClassName('payment-form__select-list')[0].classList.toggle('payment-form__select-list--active');
 };
